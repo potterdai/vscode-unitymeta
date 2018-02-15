bde5913bbaf2a5a1c51b8999462a107dff6c98ff
Potter Dai <potterdaica@gmail.com>
2018-02-15 12:50:53 -0500

    Remove unused reference. Update README.

--- src/extension.ts
+++ src/extension.ts
@@ -2,8 +2,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import * as path from 'path';
-import * as fs from 'fs';
 import * as metafilewatcher from './metafilewatcher';
 
 // this method is called when your extension is activated
