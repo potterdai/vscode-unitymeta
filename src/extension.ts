9a9cede4073bf7023fd4d79f07da01a7a06cca64
Potter Dai <potterdaica@gmail.com>
2019-07-09 19:28:13 -0400

    Revise UnityProject detection logic.

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,20 @@
-'use strict';
+"use strict";
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-import * as metafilewatcher from './metafilewatcher';
+import * as vscode from "vscode";
+import * as metafilewatcher from "./metafilewatcher";
+import * as fs from "fs";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-    vscode.workspace.findFiles("**/*.meta", null, 1).then(ps => {
-        if (ps.length > 0) {
-            //Is Unity Project
-            metafilewatcher.activate(context);
-        }
-    })
+    if (
+        fs.existsSync(vscode.workspace.rootPath + "/Library") &&
+        fs.existsSync(vscode.workspace.rootPath + "/Assets") &&
+        fs.existsSync(vscode.workspace.rootPath + "/ProjectSettings")
+    ) {
+        metafilewatcher.activate(context);
+    }
 }
 
 // this method is called when your extension is deactivated
